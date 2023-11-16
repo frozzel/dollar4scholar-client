d4e1491f805619a8c0855d1d153e86bdce12350b
Dennis Hickox <frozzel@me.com>
2023-11-16 12:06:37 -0500

    added process env for stripe at production level

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import.meta.env.VITE_APP_API
+
 
 // const client = axios.create({baseURL: "http://localhost:8080/api"});
-const client = axios.create({baseURL: "https://dollar4scholar-server-q23p4.ondigitalocean.app/api"});
+const client = axios.create({baseURL: import.meta.env.VITE_APP_API});
 
 export default client;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx'
 import './index.scss'
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
