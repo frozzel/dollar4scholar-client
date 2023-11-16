424a59a0569e2af2e5a1817d30e109a2a80d896d
Dennis Hickox <frozzel@me.com>
2023-11-16 01:09:11 -0500

    test

--- src/api/client.js
+++ src/api/client.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import.meta.env.VITE_APP_API
 
 // const client = axios.create({baseURL: "http://localhost:8080/api"});
-const client = axios.create({baseURL: "https://dollar4scholar-server-q23p4.ondigitalocean.app/"});
+const client = axios.create({baseURL: "https://dollar4scholar-server-q23p4.ondigitalocean.app/api"});
 
 export default client;
