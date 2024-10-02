93c2cb3b866467f9a775f393e54b1a24a4ef5d55
Dennis Hickox <frozzel@me.com>
2024-10-02 09:50:18 -0400

    reset back to external

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { AcceptHosted } from 'react-acceptjs';
+// import { AcceptHosted } from 'react-acceptjs';
 import {getAnAcceptPaymentPage} from '../api/payment';
 
 
@@ -22,30 +22,30 @@ function PaymentForm({userId, email, refId, amount, stripeId}) {
     , [userId, email, refId, amount, stripeId]);
 
       return formToken ? (
-              <div className="d-flex justify-content-between align-items-center px-2 ">
-              <AcceptHosted
-              className="d-flex justify-content-between align-items-center px-2 " 
-              variant="outline-*"
-                formToken={formToken}
-                integration="iframe"
-                settingName="hostedPaymentCustomerOptions"
-                onTransactionResponse={(response) =>
-                  setResponse(JSON.stringify(response, null, 2) + '\n')
-                }
-              >
-                <AcceptHosted.Button  className="getstarted2 " variant="outline-*" >
-                  Subscribe
-                </AcceptHosted.Button >
-                <AcceptHosted.IFrameBackdrop />
-                <AcceptHosted.IFrameContainer>
-                  <AcceptHosted.IFrame />
-                </AcceptHosted.IFrameContainer>
-              </AcceptHosted>
-              </div>
-          // <form method="post" action="https://test.authorize.net/payment/payment" id="formAuthorizeNetTestPage" name="formAuthorizeNetTestPage">
-          //   <input type="hidden" name="token" value={formToken} />
-          //    <button id="btnContinue"  className="getstarted2 border-0 ">Subscribe</button>
-          //  </form>         
+              // <div className="d-flex justify-content-between align-items-center px-2 ">
+              // <AcceptHosted
+              // className="d-flex justify-content-between align-items-center px-2 " 
+              // variant="outline-*"
+              //   formToken={formToken}
+              //   integration="iframe"
+              //   settingName="hostedPaymentCustomerOptions"
+              //   onTransactionResponse={(response) =>
+              //     setResponse(JSON.stringify(response, null, 2) + '\n')
+              //   }
+              // >
+              //   <AcceptHosted.Button  className="getstarted2 " variant="outline-*" >
+              //     Subscribe
+              //   </AcceptHosted.Button >
+              //   <AcceptHosted.IFrameBackdrop />
+              //   <AcceptHosted.IFrameContainer>
+              //     <AcceptHosted.IFrame />
+              //   </AcceptHosted.IFrameContainer>
+              // </AcceptHosted>
+              // </div>
+          <form method="post" action="https://test.authorize.net/payment/payment" id="formAuthorizeNetTestPage" name="formAuthorizeNetTestPage">
+            <input type="hidden" name="token" value={formToken} />
+             <button id="btnContinue"  className="getstarted2 border-0 ">Subscribe</button>
+           </form>         
             
             ) : (
               <div>
