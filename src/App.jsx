0cb208406a484094b62581276d0a6b9ca7135830
Dennis Hickox <frozzel@me.com>
2024-10-02 09:40:40 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import AdminWinnerInfo from "./pages/AdminWinnerInfo";
 import CheckoutForm from "./components/CheckoutForm";
 import CheckoutFormDonor from "./components/CheckoutFormDonor";
 import Return from "./components/Return";
-import Frame from "./pages/Frame";
+import Frame from "./pages/Frame.jsx";
 
 
 function App() {
